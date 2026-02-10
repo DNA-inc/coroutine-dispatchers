575b923fc9862e8f91cfaa20693f36acff143039
Remco Mokveld <remco.mokveld@dna.inc>
2026-02-10 09:40:55 +0100

    Add extension mehtod on TestScope

--- dispatchers-test/src/commonMain/kotlin/inc/dna/coroutines/test/TestScope.kt
+++ dispatchers-test/src/commonMain/kotlin/inc/dna/coroutines/test/TestScope.kt
@@ -1,8 +1,12 @@
 package inc.dna.coroutines.test
 
+import inc.dna.coroutines.DispatchersContextElement
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest as realRunTest
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 /** typealias to allow linters to flag all imports of [kotlinx.coroutines.test.TestScope]. */
 typealias TestScope = kotlinx.coroutines.test.TestScope
@@ -14,4 +18,18 @@ typealias TestScope = kotlinx.coroutines.test.TestScope
 @OptIn(ExperimentalCoroutinesApi::class)
 fun TestScope(context: CoroutineContext = EmptyCoroutineContext): TestScope {
   return kotlinx.coroutines.test.TestScope(createTestContext(context))
+}
+
+/**
+ * Wrapper around the [kotlinx.coroutines.test.runTest] extension method of [TestScope]
+ */
+fun TestScope.runTest(
+  timeout: Duration = 10.seconds,
+  testBody: suspend kotlinx.coroutines.test.TestScope.() -> Unit
+) {
+  check(coroutineContext[DispatchersContextElement] != null) {
+    "TestScope does not contain a DispatchersContextElement which is required to use dispatcher" +
+      " injection."
+  }
+  this.realRunTest(timeout, testBody)
 }
