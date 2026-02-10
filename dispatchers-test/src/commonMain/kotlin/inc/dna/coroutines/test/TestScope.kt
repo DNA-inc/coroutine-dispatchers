e298ac8cb196cda3f0429001dbf5e178c6121ea8
Remco Mokveld <remco.mokveld@dna.inc>
2026-02-10 11:20:50 +0100

    Formatting fix

--- dispatchers-test/src/commonMain/kotlin/inc/dna/coroutines/test/TestScope.kt
+++ dispatchers-test/src/commonMain/kotlin/inc/dna/coroutines/test/TestScope.kt
@@ -3,10 +3,10 @@ package inc.dna.coroutines.test
 import inc.dna.coroutines.DispatchersContextElement
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest as realRunTest
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest as realRunTest
 
 /** typealias to allow linters to flag all imports of [kotlinx.coroutines.test.TestScope]. */
 typealias TestScope = kotlinx.coroutines.test.TestScope
@@ -20,16 +20,14 @@ fun TestScope(context: CoroutineContext = EmptyCoroutineContext): TestScope {
   return kotlinx.coroutines.test.TestScope(createTestContext(context))
 }
 
-/**
- * Wrapper around the [kotlinx.coroutines.test.runTest] extension method of [TestScope]
- */
+/** Wrapper around the [kotlinx.coroutines.test.runTest] extension method of [TestScope] */
 fun TestScope.runTest(
-  timeout: Duration = 10.seconds,
-  testBody: suspend kotlinx.coroutines.test.TestScope.() -> Unit
+    timeout: Duration = 10.seconds,
+    testBody: suspend kotlinx.coroutines.test.TestScope.() -> Unit
 ) {
   check(coroutineContext[DispatchersContextElement] != null) {
     "TestScope does not contain a DispatchersContextElement which is required to use dispatcher" +
-      " injection."
+        " injection."
   }
   this.realRunTest(timeout, testBody)
 }
